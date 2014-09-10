5657aa87d5de2cc0e6964516a28391d80fe99f1f
SirPidgin <sirpidgin@outlook.com>
2014-09-10 15:27:53 +0300

    Menutekstit

--- Steamer/main.c
+++ Steamer/main.c
@@ -3,7 +3,9 @@
 int main()
 {
 	printf("STEAMER\n\n\n");
-	printf("Steamerin kayttoa varten tarvitset steam id:t.");
+	printf("Steamerin kayttoa varten tarvitset kahden eri profiilin steam ID:t. Ne ovat muotoa steamcommunity.com/id/pidgin, eli tassa tapauksessa ID = pidgin.\n\nValitse suoritettava komento syottamalla numero:\n\n");
+	printf("1. ID:n syotto\n2. Hae tiedot\n3. Lopeta\n\n");
+
 	printf("%d\n", aliohjelma(24));
 
 	return 0;
