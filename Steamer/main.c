16f1eab777dc5907bd81d245b738691a94121689
SirPidgin <sirpidgin@outlook.com>
2014-09-10 15:20:50 +0300

    Virhe korjattu :)

--- Steamer/main.c
+++ Steamer/main.c
@@ -3,7 +3,7 @@
 int main()
 {
 	printf("STEAMER\n\n\n");
-	printf("Steamerin kayttoa varten tarvitset steam id:t.")
+	printf("Steamerin kayttoa varten tarvitset steam id:t.");
 	printf("%d\n", aliohjelma(24));
 
 	return 0;
